75337f10dac0a1f0d93d08d95fa3d945420546fb
Pazz <pazz@MacBook-Air-Pysiorek.local>
2020-07-16 01:55:33 +0100

    Readme update

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
-import NotFound from "./containers/NotFound";
-import Login from "./containers/Login";
-import Signup from "./containers/Signup";
-import NewNote from "./containers/NewNote";
-import Notes from "./containers/Notes";
-import Settings from "./containers/Settings";
+
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Notes from "./containers/Notes";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Settings from "./containers/Settings";
+import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
@@ -16,24 +18,25 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
-      <Route exact path="/login">
+      <UnauthenticatedRoute exact path="/login">
         <Login />
-      </Route>
-      <Route exact path="/signup">
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
         <Signup />
-      </Route>
-      <Route exact path="/notes/new">
-        <NewNote />
-      </Route>
-      <Route exact path="/notes/:id">
-        <Notes />
-      </Route>
-      <Route exact path="/settings">
+      </UnauthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings">
         <Settings />
-      </Route>
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
+      {/* Finally, catch all unmatched routes */}
       <Route>
         <NotFound />
       </Route>
-      </Switch>
-  )
+    </Switch>
+  );
 }
